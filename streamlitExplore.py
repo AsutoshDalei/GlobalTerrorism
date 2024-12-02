60d05e31b96f144fe081c7d36496f0920e26b832
AsutoshDalei <asutoshdalei@gmail.com>
2024-12-01 23:27:48 -0500

    added boxplot

--- streamlitExplore.py
+++ streamlitExplore.py
@@ -3,6 +3,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.express as px
 import time
+import os
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -11,7 +12,6 @@ warnings.filterwarnings("ignore")
 def dateFnx(row):
     date = str(row['Month'])+':'+str(row['Day'])+':'+str(row['Year'])
     return pd.to_datetime(date,format='%m:%d:%Y')
-
 
 
 @st.cache_data
@@ -29,14 +29,14 @@ def fetch_and_clean_data(path,nrows = 10000):
     data['date'] = data.apply(dateFnx,axis=1)
     return data
 
-path = "/Users/asutoshdalei/Desktop/Work/GlobalTerrorism/globalterrorismdb.csv"
-data = fetch_and_clean_data(path,nrows= -1)
+data_path = "/Users/asutoshdalei/Desktop/Work/GlobalTerrorism/globalterrorismdb.csv"
+data = fetch_and_clean_data(data_path,nrows= -1)
 
 countryWise = data.groupby('Country')['casualities'].sum()
 countryList = countryWise[countryWise>5].index.tolist()
 
 # Streamlit section
-st.title("Global Terrorism Impact")
+st.title("Analysis of Global Terrorism")
 
 minTime,maxTime = data[data.latitude.notna() & data.longitude.notna()]['date'].agg(['min','max'])
 dateRangeOptions = pd.date_range(minTime,maxTime,freq = 'W').strftime('%Y-%m-%d').tolist()
@@ -45,22 +45,18 @@ start_time,end_time = st.select_slider(label='Date Range',options=dateRangeOptio
 
 ## Sidebar
 with st.sidebar:
-    # imgPath = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dglobal%2Bterrorism&psig=AOvVaw1at4COBS15aJmBDExfObUY&ust=1732377795838000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNj_1ayo8IkDFQAAAAAdAAAAABAE'
-    # st.image(imgPath)
-    st.header("Global Terrorism Analysis")
-    # st.subheader('Select Analysis Options')
+    st.title("Analysis of Global Terrorism")
+    imgPath = '/Users/asutoshdalei/Desktop/Work/GlobalTerrorism/terrorismlogo.jpg'
+    st.image(imgPath)
 
-    # mapFlag = st.toggle(label = "Region/Country Toggle", value = True)
     mapFlagCol = st.radio(label="Geography:",options=['Region','Country'])
     variable_radio = st.radio(label="Analysis Measure:",options=['Attacks','Casualities'])
     
     if mapFlagCol == 'Region':
-        # mapFlagCol = 'Region'
         mapSelection = st.multiselect(label='Select Region',options = data.Region.unique().tolist()+['Global'],default='Global')
         if ('Global' in mapSelection) or (mapSelection==[]):
             mapSelection = data.Region.unique().tolist()
     else:
-        # mapFlagCol = 'Country'
         mapSelection = st.multiselect(label='Select Country',options = data.Country.unique().tolist()+['Global'],default='Global')
         if ('Global' in mapSelection) or (mapSelection==[]):
             mapSelection = data.Country.unique().tolist()
@@ -90,10 +86,7 @@ dft3 = mapData.groupby('Country')['Region'].agg(['unique']).apply(lambda x:x[0][
 dft = pd.merge(left=dft1,right=dft2,on='Country')
 dft = pd.merge(left=dft,right=dft3,on='Country')
 
-if chartTypeLog_toggle:
-    fig = px.scatter(dft,x='casualities_sum',y='num_attacks',color='Region',hover_name="Country",log_x=True,log_y=True,size_max=60) 
-else:
-    fig = px.scatter(dft,x='casualities_sum',y='num_attacks',color='Region',hover_name="Country",log_x=False,log_y=False,size_max=60)
+fig = px.scatter(dft,x='casualities_sum',y='num_attacks',color='Region',hover_name="Country",log_x=chartTypeLog_toggle,log_y=chartTypeLog_toggle,size_max=60) 
 fig.update_layout(title="Number of attacks v/s Casualities at a nation level")
 fig.update_xaxes(title_text="Casualities")
 fig.update_yaxes(title_text="Number of attacks")   
@@ -106,7 +99,7 @@ st.divider()
 st.header(f'Changes in terrorism cases over time.')
 # freq = st.select_slider('Select Frequency:',options = ['Week','Month','Quarter','Year'])
 col1, col2 = st.columns(2)
-with col1:
+with col1:  
     freq = st.segmented_control("Select Frequency",options=['Week','Month','Quarter','Year'],selection_mode="single",default='Week')
 with col2:
     chartType_radio = st.radio(label="Chart Type:",options=['Line','Area'])
@@ -114,19 +107,11 @@ with col2:
 freqMap = {'Week':'W','Month':'M','Quarter':'Q','Year':'Y'}
 
 
-if variable_radio=='Cases':
-    # freqCount = data[['casualities','date']].set_index('date').resample(freqMap[freq])['casualities'].count()
+if variable_radio=='Attacks':
     freqCount = mapData[['casualities','date']].set_index('date').resample(freqMap[freq])['casualities'].count()
 else:
-    # freqCount = data[['casualities','date']].set_index('date').resample(freqMap[freq])['casualities'].sum()
     freqCount = mapData[['casualities','date']].set_index('date').resample(freqMap[freq])['casualities'].sum()
-
-if chartType_radio == 'Line':
-    fig = px.line(freqCount)
-    # st.line_chart(data=freqCount,color='#bf3228')
-else:
-    # st.area_chart(data=freqCount,color='#bf3228')
-    fig = px.area(freqCount,)
+fig = px.area(freqCount)
 st.plotly_chart(fig)
 
 ## Weapon Details
@@ -142,18 +127,26 @@ col1, col2 = st.columns(2)
 with col1:
     valCount1 = mapData['AttackType'].value_counts(sort=True).head(10)
     st.subheader("Attack Type")
-    st.bar_chart(data=valCount1,horizontal = True, color = '#ad331a')
-    # fig = px.bar(valCount1)
-    # fig.update_traces(textfont_size=12, textangle=45, textposition="outside", cliponaxis=False)
-    # st.plotly_chart(fig)
+    st.bar_chart(data=valCount1,horizontal = True, color = '#59ceeb')
 
 with col2:
     valCount2 = mapData['Group'].value_counts(sort=True).head(10)
     st.subheader("Attacking Group")
-    st.bar_chart(data=valCount2,horizontal = True, color = '#ada11a')
-    # fig = px.bar(valCount2)
-    # fig.update_traces(textfont_size=12, textangle=45, textposition="outside", cliponaxis=False)
-    # st.plotly_chart(fig)
+    st.bar_chart(data=valCount2,horizontal = True, color = '#59ceeb')
 
+st.divider()
+st.header("Casualities v/s Target Types")
+outlier_toggle = st.toggle(label = "Outlier Analysis", value = True)
 
+if outlier_toggle:
+    fig = px.box(data_frame=mapData,x='Target_type',y='casualities',log_y=True,points='suspectedoutliers')
+    fig.update_yaxes(title_text="Casualities in Log")
+else:
+    fig = px.box(data_frame=mapData,x='Target_type',y='casualities',log_y=False,points=False)
+    fig.update_yaxes(title_text="Casualities")
+fig.update_xaxes(title_text="Target Type")
+st.plotly_chart(fig)
 
+st.divider()
+st.header("Terrorism in Images")
+imgs = os.listdir()
